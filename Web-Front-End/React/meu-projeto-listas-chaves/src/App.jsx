4d64f25c6e68b1c0c0a97eaf7be1261626b685da
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-05-26 22:54:33 -0300

    Update: Santander Coders 2023 - Listas e Chaves

---
+++ Web-Front-End/React/meu-projeto-listas-chaves/src/App.jsx
@@ -0,0 +1,16 @@
+import MinhaLista from "./components/MinhaLista"
+
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>Listas no React</h1>
+      <ul>
+        <MinhaLista/>
+      </ul>
+    </div>
+  )
+}
+
+App
